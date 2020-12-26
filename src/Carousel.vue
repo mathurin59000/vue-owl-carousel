e0a1307f5ae5de5ac78eeeaf222a7d4c8c4727bc
Ramart Mathurin <mathurin.ramart@isen-lille.fr>
2020-12-26 17:02:08 +0100

    fix: click event after touch drag

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -14,7 +14,7 @@
 <script>
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import 'owl.carousel';
+import './owl.carousel';
 
 import events from './utils/events';
 
